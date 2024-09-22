eeb55381e1eb13937478de3b9fab4b8e3a0a9fa4
Srivastavasanjh123 <sanjhalisrivastava3@gmail.com>
2024-09-22 15:48:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import ServiceForm from './components/ServiceForm';
 import ServiceList from './components/ServiceList';
@@ -8,12 +7,12 @@ function App() {
   const [services, setServices] = useState([]);
   const [serviceToEdit, setServiceToEdit] = useState(null); // Holds the service that is being edited
 
-  // Add a new service
+  // Adding a new service
   const addService = (service) => {
     setServices([...services, service]);
   };
 
-  // Update an existing service
+  // Updating an existing service
   const updateService = (updatedService) => {
     const updatedServices = services.map((service, index) =>
       index === serviceToEdit.index ? updatedService : service
@@ -22,7 +21,7 @@ function App() {
     setServiceToEdit(null); // Reset after editing
   };
 
-  // Delete a service from the list
+  // Deleting a service from the list
   const deleteService = (index) => {
     const updatedServices = services.filter((_, i) => i !== index);
     setServices(updatedServices);
